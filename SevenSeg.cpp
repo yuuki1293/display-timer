9312b378e320417a34cd24ab5bd7ee1ac85163ee
yuuki1293 <yuuki1293@outlook.jp>
2022-05-15 23:50:12 +0900

    Seg4

--- SevenSeg.cpp
+++ SevenSeg.cpp
@@ -93,3 +93,7 @@ void SevenSeg::draw2(int x, int y) const {
 void SevenSeg::draw3(int x, int y) const {
     drawpart(x, y, 1, 1, 1, 1, 0, 0, 1, 0);
 }
+
+void SevenSeg::draw4(int x, int y) const {
+    drawpart(x, y, 0, 1, 1, 0, 0, 1, 1, 0);
+}
